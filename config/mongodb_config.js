ebc9ca6608208c42932e6ab4c6cfea0f8370b4cf
uka yura <ukayura@ukas-MacBook-Air.local>
2023-04-02 12:13:49 +0800

    uka hiisen blno

--- config/mongodb_config.js
+++ config/mongodb_config.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
 
-mongoose
-  .connect(
+try {
+  mongoose.connect(
     `mongodb+srv://uka:Mongol0318@ecomme.8qlwd64.mongodb.net/ecomme`
-  ).then(() => console.log("connect"));
+  );
+  console.log("connected");
+} catch (error) {
+  console.log("not connected");
+}
 
 export default mongoose.connection;
